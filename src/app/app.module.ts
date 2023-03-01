c9919a2d8e2e25e248a19c93664afb61e62e540c
Andre Carter <andre.carter@knights.ucf.edu>
2023-03-01 01:18:56 -0500

    added svg icons/services/details component data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,14 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { HomeComponent } from './components/home/home.component';
 import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptors';
 import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptors';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    HomeComponent
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +29,7 @@ import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptors';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    GaugeModule,
+    GaugeModule.forRoot(),
     MatTabsModule,
     MatIconModule,
     MatFormFieldModule,
